98967e42362517850958b6593ded8b47c6d0afa5
Jerlin Ishabel J <ishaa5034@gmail.com>
2025-08-10 19:19:35 +0530

    Delete ecommerce-product-page directory

--- ecommerce-product-page/script.js
+++
@@ -1,9 +0,0 @@
-function addToCart(productName) {
-  const message = document.getElementById("cart-message");
-  message.innerText = `${productName} has been added to your cart! ✅`;
-
-  // Auto-hide after 3 seconds
-  setTimeout(() => {
-    message.innerText = "";
-  }, 3000);
-}
